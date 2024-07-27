abe89950bdfa3afa5e592b959a3a8cf67f55ad5d
odiegosilva1 <odiegosilvadev@gmail.com>
2024-07-27 10:12:31 -0300

    add: get method

--- axios-vite/src/App.jsx
+++ axios-vite/src/App.jsx
@@ -1,22 +1,31 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import axios from "axios";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   //state
   const [pokemonList, setPokemonList] = useState([]);
 
-  axios
-    .get(" https://pokeapi.co/api/v2/pokemon/")
-    .then((res) => {
-      console.log(res.data.results);
-      setPokemonList(res.data.results);
-    })
-    .catch((err) => console.log(err));
+  // Chama apenas uma vez
+  useEffect(() => {
+    axios
+      .get(" https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => {
+        setPokemonList(res.data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  return <div className="App">API Pokémon</div>;
+  return (
+    <div className="App">
+      {pokemonList.map((pokemon) => (
+        <div>{pokemon.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
